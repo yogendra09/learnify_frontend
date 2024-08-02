a60fd9671b5f61d389b51c8f7ed212acd83f9da6
shivam kumar shaha <shivamshah1210@gmail.com>
2024-08-02 13:24:25 +0530

    final change

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://backend-gmmc.onrender.com',
+    baseURL: 'https://backend-gmmc.onrender.com/',
     withCredentials:true
 
 })
