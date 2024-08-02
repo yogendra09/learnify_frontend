783cfb43d20357487814a8c1747bd0e362c81c97
yogendra09 <yogendrayadav168@gmail.com>
2024-08-02 16:35:20 +0530

    love

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://backend-gmmc.onrender.com/',
+    baseURL: 'https://learnify-backend-bmrz.onrender.com',
     withCredentials:true
 
 })
