7981f35e7465bbc95f7fe70856dccebda7717968
yogendra09 <yogendrayadav168@gmail.com>
2024-08-02 16:16:14 +0530

    commit

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -11,7 +11,7 @@ const Courses = () => {
 
   return (
     <div className="w-full py-20 flex items-start justify-center gap-5 flex-wrap mx-auto px-20 max-sm:w-full max-sm:px-5">
-      {allcourse.length > 0 ? allcourse.map((course) => (
+      {allcourse && allcourse.length > 0 ? allcourse.map((course) => (
         <Coursescard course={course} key={course._id} admin={admin} id={course._id} />
       )) : <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfUnw-XV8gvRIhoXrubp-8iTz4qkDWZkuKkg&s" alt="" /> }
     </div>
